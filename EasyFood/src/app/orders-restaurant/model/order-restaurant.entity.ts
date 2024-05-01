059b969bdcb399dfadbc852e96d5bcdfe50a3499
ErickVasquezz <89364442+ErickVasquezz@users.noreply.github.com>
2024-05-01 03:24:15 -0400

    Web-Erick

---
+++ EasyFood/src/app/orders-restaurant/model/order-restaurant.entity.ts
@@ -0,0 +1,10 @@
+export class OrderRestaurant{
+  id: number;
+  customer: string;
+  nOrders: number;
+  constructor() {
+    this.id = 0;
+    this.customer = "";
+    this.nOrders = 0;
+  }
+}
---
+++ EasyFood/src/app/public/pages/order/order.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {CardLunchComponent} from "../../../orders/components/card-lunch/card-lunch.component";
+import {CardDrinkComponent} from "../../../orders/components/card-drink/card-drink.component";
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrl: './order.component.css'
+})
+export class OrderComponent {
+
+}
