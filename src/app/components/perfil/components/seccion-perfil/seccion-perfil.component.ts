53e4a5cc3108ada2e5215e13826514d29170199a
Juan Diego Huaman Monzón <u202123965@upc.edu.pe>
2024-06-04 02:54:57 -0500

    Add profile-restaurant component

---
+++ src/app/components/perfil/components/seccion-perfil/seccion-perfil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-seccion-perfil',
+  templateUrl: './seccion-perfil.component.html',
+  styleUrl: './seccion-perfil.component.css'
+})
+export class SeccionPerfilComponent {
+
+  constructor() {
+  }
+
+}
