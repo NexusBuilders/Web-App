fe14733a2602e35a79ca93b4cd9a694c0f97fc32
Juan Diego Huaman Monzón <u202123965@upc.edu.pe>
2024-06-23 16:21:43 -0500

    feat(order): udpate drinks and lunches component

--- src/app/orders/services/drink.service.ts
+++ src/app/orders/services/drink.service.ts
@@ -1,16 +1,34 @@
-import {Injectable} from "@angular/core";
-import {BaseService} from "../../shared/services/base.service";
-import {HttpClient} from "@angular/common/http";
-import {Drink} from "../model/drink.entity";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Drink } from '../model/drink.entity';
+import { TokenStorageService } from '../../shared/services/token.service';
+
+const BASE_URL = 'http://191.239.123.6';
+const DRINK_API = BASE_URL + '/api/v1/drinks/restaurant/';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DrinkService extends BaseService<Drink>{
-  constructor(http: HttpClient) {
-    super(http);
-    this.resourceEndpoint= 'api/v1/drinks/restaurant/1'
+export class DrinkService {
+  constructor(private http: HttpClient, private tokenStorage: TokenStorageService) {}
+
+  getDrinks(restaurantId: number): Observable<Drink[]> {
+    const token = this.tokenStorage.getToken();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      })
+    };
+    return this.http.get<Drink[]>(`${DRINK_API}${restaurantId}`, httpOptions).pipe(
+      catchError(this.handleError)
+    );
   }
 
+  private handleError(error: any): Observable<never> {
+    console.error('An error occurred', error);
+    return throwError(error.message || error);
+  }
 }
-
--- src/app/orders/services/lunch.service.ts
+++ src/app/orders/services/lunch.service.ts
@@ -1,14 +1,34 @@
-import {Injectable} from "@angular/core";
-import {BaseService} from "../../shared/services/base.service";
-import {HttpClient} from "@angular/common/http";
-import {Lunch} from "../model/lunch.entity";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Lunch } from '../model/lunch.entity';
+import { TokenStorageService } from '../../shared/services/token.service';
+
+const BASE_URL = 'http://191.239.123.6';
+const LUNCH_API = BASE_URL + '/api/v1/lunches/restaurant/';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LunchService extends BaseService<Lunch>{
-  constructor(http: HttpClient) {
-    super(http);
-    this.resourceEndpoint= '/api/v1/lunches/restaurant/1'
+export class LunchService {
+  constructor(private http: HttpClient, private tokenStorage: TokenStorageService) {}
+
+  getLunches(restaurantId: number): Observable<Lunch[]> {
+    const token = this.tokenStorage.getToken();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      })
+    };
+    return this.http.get<Lunch[]>(`${LUNCH_API}${restaurantId}`, httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: any): Observable<never> {
+    console.error('An error occurred', error);
+    return throwError(error.message || error);
   }
 }
