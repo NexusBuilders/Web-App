6d539cefb20f831cb45771e63dbac12158bd0058
Juan Diego Huaman Monzón <u202123965@upc.edu.pe>
2024-06-23 13:06:14 -0500

    feat(userprofile): update userProfile component

--- src/app/components/userprofile/userprofile.component.ts
+++ src/app/components/userprofile/userprofile.component.ts
@@ -1,98 +1,60 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
-import {UserService} from "../../shared/services/user.service";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from "@angular/router";
+import { UserService } from "../../shared/services/user.service";
+import {TokenStorageService} from "../../shared/services/token.service";
 
 @Component({
   selector: 'app-profile',
-  templateUrl: 'userprofile.component.html',
-  styleUrls: ['userprofile.component.css']
+  templateUrl: './userprofile.component.html',
+  styleUrls: ['./userprofile.component.css']
 })
-export class UserProfileComponent {
-
-  nombreUsuario: string = '';
-  nombre: string = '';
-  apellido: string = '';
-  correoElectronico: string = '';
-  telefono: string = '';
-  nombreEditado: string = '';
-  correoEditado: string = '';
-  editandoNombre: boolean = false;
-  editandoCorreo: boolean = false;
-  fotoPerfil: string = '';
+export class UserProfileComponent implements OnInit {
+  profileForm!: FormGroup;
   showPopup = false;
 
-  constructor(private userService: UserService, private router: Router) {}
-  menuItems = [
-    { icon: 'fa fa-user', text: 'Mis Datos' },
-    { icon: 'fa fa-lock', text: 'Acceso y Seguridad' },
-    { icon: 'fa fa-cog', text: 'Configuración' },
-    { icon: 'fa fa-question-circle', text: 'Ayuda' }
-  ];
-   showProfilePopup() {
-     this.showPopup = !this.showPopup;
-   }
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private tokenStorage: TokenStorageService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.profileForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required]
+    });
+
+    this.loadUserProfile();
+  }
+
+  loadUserProfile(): void {
+    const user = this.tokenStorage.getUser();
+    const token = this.tokenStorage.getToken();
+    if (user && user.id && token) {
+      this.userService.getUserProfile(user.id, token).subscribe(
+        data => {
+          this.profileForm.patchValue({
+            firstName: data.firstName,
+            lastName: data.lastName,
+            email: data.email,
+            phone: data.phone
+          });
+          console.log(data);
+        },
+        error => {
+          console.error('Error loading user profile', error);
+        }
+      );
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  showProfilePopup(): void {
+    this.showPopup = !this.showPopup;
+  }
 }
-
-
-  //
-  // onSelect(item: any): void {
-  //   console.log('Selected:', item.text);
-  // }
-  //
-  // ngOnInit(): void {
-  //   const usuarioLogueado = this.userService.getUsuarioLogueado();
-  //   if (usuarioLogueado) {
-  //     this.nombreUsuario = usuarioLogueado.nombre + ' ' + usuarioLogueado.apellido;
-  //     this.correoElectronico = usuarioLogueado.correo;
-  //     this.telefono = usuarioLogueado.telefono;
-  //     this.fotoPerfil = usuarioLogueado.perfil;
-  //   }
-  // }
-
-  //
-  //
-  // guardarNombre() {
-  //   this.nombreUsuario = this.nombreEditado;
-  //   this.editandoNombre = false;
-  //
-  //   const usuarioLogueado = this.userService.getUsuarioLogueado();
-  //   usuarioLogueado.nombre = this.nombreUsuario;
-  //   this.userService.editarUsuario(usuarioLogueado).subscribe(response => {
-  //     if (response) {
-  //       console.log('Nombre de usuario actualizado en el servidor');
-  //     } else {
-  //       console.error('Error al actualizar el nombre de usuario');
-  //     }
-  //   });
-  // }
-  //
-  // guardarCorreo() {
-  //   this.correoElectronico = this.correoEditado;
-  //   this.editandoCorreo = false;
-  //
-  //   const usuarioLogueado = this.userService.getUsuarioLogueado();
-  //   usuarioLogueado.correo = this.correoElectronico;
-  //   this.userService.editarUsuario(usuarioLogueado).subscribe(response => {
-  //     if (response) {
-  //       console.log('Correo electrónico actualizado en el servidor');
-  //     } else {
-  //       console.error('Error al actualizar el correo electrónico');
-  //     }
-  //   });
-  // }
-  //
-  // editarNombre() {
-  //   this.nombreEditado = this.nombreUsuario;
-  //   this.editandoNombre = true;
-  // }
-  //
-  // editarCorreo() {
-  //   this.correoEditado = this.correoElectronico;
-  //   this.editandoCorreo = true;
-  // }
-  //
-  // cerrarSesion(){
-  //   this.userService.cerrarSesion();
-  //   this.router.navigate(['/iniciosesion']);
-  // }
-
